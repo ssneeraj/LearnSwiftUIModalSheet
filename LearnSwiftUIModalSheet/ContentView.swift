c69033b2a50d3551d09d51d3aedcae3c78707655
Neeraj Sharma <neeraj_sharma@starkey.com>
2020-11-02 14:44:43 -0600

    Initial Commit

---
+++ LearnSwiftUIModalSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnSwiftUIModalSheet
+//
+//  Created by Neeraj Sharma on 11/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearnSwiftUIModalSheet/LearnSwiftUIModalSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIModalSheetApp.swift
+//  LearnSwiftUIModalSheet
+//
+//  Created by Neeraj Sharma on 11/2/20.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIModalSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
